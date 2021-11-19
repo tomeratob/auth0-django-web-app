040f85dde75bba17c022534c0b2f04c7c16c7db4
tomer@ownbackup.com <tomer@ownbackup.com>
2021-11-19 15:33:52 +0200

    salesforce oauth

--- 01-Login/auth0login/apps.py
+++ 01-Login/auth0login/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from social_django.config import PythonSocialAuthConfig
 
 
 class Auth0LoginConfig(AppConfig):
     name = 'auth0login'
+    PythonSocialAuthConfig.default_auto_field = 'django.db.models.BigAutoField'
